5d44c1e376d9729dfd9f2221f43df298cbd56db0
naokiur <atomic.transfer.comp@gmail.com>
2020-03-14 16:54:58 +0900

    fix #1.

--- app/src/main/java/jp/ne/naokiur/lenstimer/MainActivity.kt
+++ app/src/main/java/jp/ne/naokiur/lenstimer/MainActivity.kt
@@ -19,43 +19,43 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val lensDao = LensDao(this)
-
-        val lenses = lensDao.selectAll()
-
-        if (lenses.count() == 0) {
-            val intent = Intent(this, InitialSettingActivity::class.java)
-            startActivity(intent)
-        }
-
-//        val navView: BottomNavigationView = findViewById(R.id.nav_view)
-
-        val right = lensDao.selectLensByForEye("R")
-        val left = lensDao.selectLensByForEye("L")
-
-        println("${right.productName}, ${right.lensPower}, ${right.forEye}")
-        println("${left.productName}, ${left.lensPower}, ${left.forEye}")
-
-        leftEyeButton = findViewById(R.id.button2)
-        rightEyeButton = findViewById(R.id.button)
-
-        leftEyeButton.setOnClickListener {
-            val result = lensDao.selectAll()
-
-            result.forEach {
-                println("${it.productName}, ${it.lensPower}, ${it.forEye}")
-            }
-        }
-
-        rightEyeButton.setOnClickListener {
-            val lens = Lens(
-                "test",
-                BigDecimal(1.0),
-                "R",
-                Calendar.getInstance().timeInMillis
-            )
-            lensDao.insert(lens)
-        }
+//        val lensDao = LensDao(this)
+//
+//        val lenses = lensDao.selectAll()
+//
+//        if (lenses.count() == 0) {
+//            val intent = Intent(this, InitialSettingActivity::class.java)
+//            startActivity(intent)
+//        }
+//
+////        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+//
+//        val right = lensDao.selectLensByForEye("R")
+//        val left = lensDao.selectLensByForEye("L")
+//
+//        println("${right.productName}, ${right.lensPower}, ${right.forEye}")
+//        println("${left.productName}, ${left.lensPower}, ${left.forEye}")
+//
+//        leftEyeButton = findViewById(R.id.button2)
+//        rightEyeButton = findViewById(R.id.button)
+//
+//        leftEyeButton.setOnClickListener {
+//            val result = lensDao.selectAll()
+//
+//            result.forEach {
+//                println("${it.productName}, ${it.lensPower}, ${it.forEye}")
+//            }
+//        }
+//
+//        rightEyeButton.setOnClickListener {
+//            val lens = Lens(
+//                "test",
+//                BigDecimal(1.0),
+//                "R",
+//                Calendar.getInstance().timeInMillis
+//            )
+//            lensDao.insert(lens)
+//        }
 
 //        textMessage = findViewById(R.id.message)
 //        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
